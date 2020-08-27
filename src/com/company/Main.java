4987b67bb75bd736e9175b71d4ac85dedd734ea1
cuker0 <marcin.cukrowski@gmaIL.COM>
2020-08-27 17:05:41 +0200

    Add two additional methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args){
-        String example = "Hello World";
-        System.out.println(example);
+
+    MobilePhone phone = new MobilePhone("200g",100);
+
+    MobilePhone oldphone = new MobilePhone("1000g",10);
+
+    MobilePhone newphone = new MobilePhone("50g",555000);
+    String parameters = phone.getParameter();
+    System.out.println(parameters);
+    oldphone.displayValues();
+
+    }
+
+    static class  MobilePhone
+
+    {
+
+        String weight = "200g";
+        int price;
+
+        public MobilePhone(String weight,int price)
+            {
+
+            this.weight = weight;
+            this.price = price;
+            }
+        //tworzenie metody zwraacajacej tekst
+        public void displayValues(){
+            System.out.println(this.weight+ " " + this.price);
+            }
+
+        // tworzenie metody zwracajacej String
+        public String getParameter() {
+            return this.weight + " "+ this.price;
+        }
     }
 }
